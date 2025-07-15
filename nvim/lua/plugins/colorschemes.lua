58ff6fac22c0496e9ee069c18bfb7254d306a019
M <markus+github@langenoja.se>
2025-07-15 21:33:12 +0200

    feat: update vim to dim non-active pane

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -9,6 +9,13 @@ return {
     config = function(_, opts)
       require("eldritch").setup(opts)
       vim.cmd.colorscheme("eldritch-dark")
+
+      local normal_hl = vim.api.nvim_get_hl(0, { name = "EndOfBuffer", link = false })
+      local bg_color = normal_hl.fg and string.format("#%06x", normal_hl.fg) or nil
+
+      if bg_color then
+        vim.api.nvim_set_hl(0, "NormalNC", { bg = bg_color })
+      end
     end,
   },
 }
