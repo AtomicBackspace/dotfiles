46c287a1addf13c6519325b07d551b86a9c47c5c
M <markus+github@langenoja.se>
2025-07-17 07:26:15 +0200

    chore: cleanup vim

--- install_mac.sh
+++ install_mac.sh
@@ -7,7 +7,6 @@ brew install \
   zsh \
   jandedobbeleer/oh-my-posh/oh-my-posh \
   stow \
-  vim \
   neovim \
   kitty \
   keepassxc \
@@ -36,14 +35,3 @@ oh-my-posh font install meslo
 
 # Install Go LSP
 go install golang.org/x/tools/gopls@latest
-
-# Install krew
-(
-  set -x; cd "$(mktemp -d)" &&
-  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
-  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
-  KREW="krew-${OS}_${ARCH}" &&
-  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
-  tar zxvf "${KREW}.tar.gz" &&
-  ./"${KREW}" install krew
-)
--- stow.sh
+++ stow.sh
@@ -11,7 +11,6 @@ mkdir -p $HOME/.config/kitty
 stow -t $HOME/.config/kitty kitty
 
 # Install VIM config and plugins
-stow -t $HOME vim 
 mkdir -p $HOME/.config/nvim
 stow -t $HOME/.config/nvim nvim 
 
@@ -28,18 +27,3 @@ stow -t $HOME/.config/k9s k9s
 # Clone repositories
 mkdir -p $HOME/.vim/pack/{plugins,tpope}/start
 mkdir -p $HOME/.vim/bundle
-
-cd $HOME/.vim/bundle
-if [ ! -d Vundle.vim ]; then
-  git clone https://github.com/VundleVim/Vundle.vim.git
-fi
-
-cd $HOME/.vim/pack/plugins/start
-if [ ! -d vim-terraform ]; then
-  git clone https://github.com/hashivim/vim-terraform.git
-fi
-
-cd $HOME/.vim/pack/tpope/start
-if [ ! -d vim-commentary ]; then
-  git clone https://github.com/tpope/vim-commentary.git
-fi
