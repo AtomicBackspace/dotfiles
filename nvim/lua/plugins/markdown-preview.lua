295bc0aa101d8a2f8311a0eed6b2729f89278619
AtomicBackspace <markus+github@langenoja.se>
2025-07-24 10:39:07 +0200

    feat: add markdown plantuml server

--- nvim/lua/plugins/markdown-preview.lua
+++ nvim/lua/plugins/markdown-preview.lua
@@ -6,6 +6,14 @@ return {
     vim.fn["mkdp#util#install"]()
   end,
   config = function()
+    vim.g.mkdp_preview_options = {
+      markdown_it_plugins = {
+        "markdown-it-plantuml"
+      },
+      uml = {
+        server = "http://127.0.0.1:8889"
+      }
+    }
     vim.g.mkdp_auto_start = 0
     vim.g.mkdp_auto_close = 1
   end,
