950ad6dd860db574aed842a4054c5ad2dd15e7e1
M <markus+github@langenoja.se>
2025-07-30 12:50:28 +0200

    chore: more configuration of autocompletion and LSPs

--- nvim-nightly/init.lua
+++ nvim-nightly/init.lua
@@ -8,6 +8,7 @@ vim.o.expandtab = true
 vim.o.autoindent = true
 vim.o.swapfile = false
 vim.o.winborder = "rounded"
+vim.o.completeopt = "menu,menuone,noselect" -- setup for autocompletion with omnifunc
 
 -- Navigation
 vim.keymap.set("n", "<Tab>", ":bn<CR>")
@@ -24,6 +25,7 @@ vim.keymap.set('n', '<leader>q', ':quit<CR>')
 vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
 vim.keymap.set({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
 
+-- Plugins
 vim.pack.add({
   { src = "https://github.com/webhooked/kanso.nvim" },  -- theme
   { src = "https://github.com/rose-pine/neovim" },      -- theme
@@ -31,16 +33,13 @@ vim.pack.add({
   { src = "https://github.com/stevearc/oil.nvim" },     -- file browser
   { src = "https://github.com/neovim/nvim-lspconfig" }, -- LSP configurations
   -- Consider adding https://github.com/stevearc/conform.nvim if formatting yaml, md etc gets annoying
-  { src = "https://github.com/Tadaa/vimade" }, -- dimming windows
+  { src = "https://github.com/Tadaa/vimade" },          -- dimming windows
 })
 
-
--- Setup plugins
 require "mini.pick".setup()
 require "oil".setup({ view_options = { show_hidden = true } })
 require "vimade".setup({ ncmode = "windows", fadelevel = 0.65 })
 
--- Keymaps for plugins
 vim.keymap.set('n', '<C-f>', ":Pick files<CR>")
 vim.keymap.set('n', '<C-g>', ":Pick grep_live<CR>")
 vim.keymap.set('n', '<C-e>', ":Oil<CR>")
@@ -49,16 +48,16 @@ vim.api.nvim_create_user_command("E", function()
   require("oil").open()
 end, {})
 
--- Enable features for LSP
-vim.cmd("set completeopt+=noselect")
+
+-- LSP Features
 local on_attach = function(client, bufnr)
   -- organize imports
   if client.id == "gopls" then
     vim.api.nvim_create_autocmd("BufWritePre", {
       buffer = bufnr,
       callback = function()
-        local params = vim.lsp.util.make_range_params()
-        params.context = { only = { "source.organizeImports" } }
+        local params = vim.lsp.util.make_range_params(bufnr, vim.lsp._get_offset_encoding())
+        params["context"] = { only = { "source.organizeImports" } }
         local result = vim.lsp.buf_request_sync(bufnr, "textDocument/codeAction", params, 3000)
         for cid, res in pairs(result or {}) do
           for _, r in pairs(res.result or {}) do
@@ -73,10 +72,13 @@ local on_attach = function(client, bufnr)
   end
 
   -- automatic formatting
-  if client.server_capabilities.documentFormattingProvider then
-    vim.api.nvim_create_autocmd("BufWritePre", {
+  if not client:supports_method('textDocument/willSaveWaitUntil')
+      and client:supports_method('textDocument/formatting') then
+    vim.api.nvim_create_autocmd('BufWritePre', {
       buffer = bufnr,
-      callback = vim.lsp.buf.format,
+      callback = function()
+        vim.lsp.buf.format({ bufnr = bufnr, id = client.id, timeout_ms = 1000 })
+      end,
     })
   end
 
@@ -92,18 +94,31 @@ local on_attach = function(client, bufnr)
     })
   end
 
+  -- document color
+  if client:supports_method('textDocument/documentColor') then
+    vim.lsp.document_color.enable(true, bufnr)
+  end
+
   -- autocompletions
   if client:supports_method('textDocument/completion') then
     -- Setup autocomplete on every keystroke
     local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
     client.server_capabilities.completionProvider.triggerCharacters = chars
+
     vim.lsp.completion.enable(true, client.id, bufnr, {
       autotrigger = true,
+      convert = function(item)
+        return { abbr = item.label:gsub('%b()', '') }
+      end,
     })
   end
 
+  -- semantic tokens
+  if client.server_capabilities.semanticTokensProvider then
+    vim.lsp.semantic_tokens.enable(true, { client.id, bufnr })
+  end
+
   -- keymaps
-  vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
   vim.keymap.set('n', '<leader>dd', vim.diagnostic.open_float)
 end
 
@@ -179,4 +194,5 @@ vim.lsp.enable({
   "tailwindcss",
 })
 
+-- Colorschema
 vim.cmd("colorscheme rose-pine-main")
